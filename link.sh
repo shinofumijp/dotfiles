2d2a7d017f97e523cbfccbec1677d9400e7fb1d9
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2018-03-19 21:15:01 +0900

    Add dein

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
+ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
