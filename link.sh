43bbca06002874fe403a9352b048febac0d677bb
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2020-01-06 10:43:28 +0900

    Add brewfile

--- link.sh
+++ link.sh
@@ -8,3 +8,4 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/dotfiles/.brew/Brewfile ~/Brewfile
